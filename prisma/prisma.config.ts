8a5b4bbabbcf942729d20dadc85096aa65bec91d
guadagattesco-alt <gattescoguadalupe@iresm.edu.ar>
2026-06-12 23:59:44 -0300

    Eliminar prisma.config.ts obsoleto de Prisma 7

--- prisma/prisma.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@prisma/config'
-
-export default defineConfig({
-  datasource: {
-    url: "mongodb+srv://gattescoquadalupe_db_user:Ue8kFOlVooARqZKg@menuexpress.xxxx.mongodb.net/test?retryWrites=true&w=majority",
-  },
-})
